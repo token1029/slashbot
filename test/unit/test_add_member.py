3eefc832ad7b763ee42cb4fd0d84a0d2a025e6e0
KingBanshou <143905049+KingBanshou@users.noreply.github.com>
2023-10-15 09:48:44 +0800

    Add files via upload

---
+++ test/unit/test_add_member.py
@@ -0,0 +1,22 @@
+import unittest
+from BaseCase import BaseCase
+class TestAddmember(BaseCase):
+    """
+    Unit test for adding custom category
+    """
+    def test_add_member(self):
+        """
+        Adding a custom category, reflects the new category in the list
+        """
+        custom_member = "bbb"
+        custom_id="0001"
+        custom_email="12333333@qq.com"
+        self.user.add_member(custom_member,custom_email,custom_id)
+        raw_content = self.user.members.keys()
+
+        userlists=[]
+        for mem in raw_content:
+            userlists.append(mem)
+
+if __name__ == '__main__':
+    unittest.main()
