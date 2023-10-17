f36de4c3fce7055df61cf4895affc88970cd48e4
KingBanshou <143905049+KingBanshou@users.noreply.github.com>
2023-10-18 04:34:52 +0800

    Update test_add_member.py

--- test/unit/test_add_member.py
+++ test/unit/test_add_member.py
@@ -4,21 +4,36 @@ class TestAddmember(BaseCase):
     """
     Unit test for adding or reducing members
     """
-    def test_add_member(self):
+    def test_add_member1(self):
         """
         Adding a custom member, reflects the new member in the list
         """
-        custom_member = "bbb"
-        custom_id="0001"
-        custom_email="12333333@qq.com"
-        self.user.add_member(custom_member,custom_email,custom_id)
+        custom_member1 = "bbb"
+        custom_id1="0001"
+        custom_email1="12333333@qq.com"
+        self.user.add_member(custom_member1,custom_email1,custom_id1)
         raw_content = self.user.members.keys()
 
         userlists=[]
         for mem in raw_content:
             userlists.append(mem)
-        assert custom_member in userlists
+        assert custom_member1 in userlists
+        
+      def test_add_member2(self):
+        """
+        Adding a custom member, reflects the new member in the list
+        """
+        custom_member2 = "1ww"
+        custom_id2="0002"
+        custom_email2="cgpu@gmail.com"
+        self.user.add_member(custom_member2,custom_email2,custom_id2)
+        raw_content = self.user.members.keys()
 
+        userlists=[]
+        for mem in raw_content:
+            userlists.append(mem)
+        assert custom_member2 in userlists
+          
     def test_delete_member(self):
         """
         deleting undesired members
